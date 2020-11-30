a55cacecf1e916b0adb2d8084853821d78b6ab35
Tomas Straka <tomas.straka@rossum.ai>
2020-11-30 13:54:34 +0100

    .env handling

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config({ path: './.env' });
 const doc = require('./utils/document');
 
 const app = express();
